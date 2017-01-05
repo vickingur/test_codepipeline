ad8a7b4d48f1dab746b95d5d119c9e9d958c4340
Victor Poputnikov <vickingur@gmail.com>
2017-01-04 19:08:03 -0800

    log in into amazon before pulling the image

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -3,4 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/.env
 
+echo "Logging in to Amazon ECR..."
+$(aws ecr get-login --region $AWS_DEFAULT_REGION)
 docker pull ${DOCKER_REPO}test_codepipeline:latest
