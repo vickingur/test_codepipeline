192250148acb18cf82558fde178f006823ce6a23
Victor Poputnikov <vickingur@gmail.com>
2017-01-04 20:24:49 -0800

    fix port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/.env
 
-docker run -it -d --name c_test_codepipeline -p 80:80 -p 5000:5000 ${DOCKER_REPO}test_codepipeline:latest
+docker run -it -d --name c_test_codepipeline -p 8080:80 -p 5000:5000 ${DOCKER_REPO}test_codepipeline:latest
